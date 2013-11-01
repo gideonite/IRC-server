660248950b7933aae2d8c612d5192a979091868d
Gideon Dresdner <gideond@gmail.com>
2013-10-31 20:46:15 -0400

    finish abnf and dump into separate file

--- src/irc_server/parser.clj
+++ src/irc_server/parser.clj
@@ -2,28 +2,10 @@
   (:require [instaparse.core :as insta]))
 
 (def request-parser
-  (insta/parser   ;; TODO plop this into its own file
-                "
-                message    =  [ ':' prefix SP ] command [ params ] crlf
-                prefix     =  servername / ( nickname [ [ '!' user ] '@' host ] )
-                command    =  1*ALPHA / 3DIGIT
-                params     =  *14( SP middle ) [ SP ':' trailing ]
-                           / 14( SP middle ) [ SP [ ':' ] trailing ]
-
-                servername = 14*ALPHA
-                nickname = servername
-                user = nickname
-                host = user
-
-                nospcrlfcl =  %x01-09 / %x0B-0C / %x0E-1F / %x21-39 / %x3B-FF
-                middle     =  nospcrlfcl *( ':' / nospcrlfcl )
-                trailing   =  *( ':' / ' ' / nospcrlfcl )
-                crlf       =  CR LF
-                "
+  (insta/parser "resources/parser-rules.abnf"
                 :input-format :abnf))
 
 (comment
-  (request-parser "NICK\r\n")
   (request-parser "NICK gideon\r\n")
   (request-parser "USER gideon gideon localhost :Gideon\r\n")
   (request-parser "join #foobar\r\n")
