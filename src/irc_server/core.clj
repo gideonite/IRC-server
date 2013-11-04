d541e60b889bb84e90401408d7bdefc6042dba5f
Gideon Dresdner <gideond@gmail.com>
2013-11-04 10:41:49 -0500

    pull out details from grammar

--- src/irc_server/core.clj
+++ src/irc_server/core.clj
@@ -1,15 +1,18 @@
 (ns irc-server.core
-  (:require [aleph.tcp :refer :all]
+  (:require [irc-server.parser :refer :all]
+            [aleph.tcp :refer :all]
             [lamina.core :refer :all]
             [gloss.core :refer :all]))
 
 (defn handler [ch client-info]
   (receive-all ch
-               #(enqueue ch (str "You said " % "\n"))))
+               ;#(enqueue ch (str "You said: " % "\n"))
+               ;#(enqueue ch (request-parser %))
+               ))
 
 (defn -main [& args]
   (println "starting up echo tcp server on port 1234")
   (start-tcp-server handler
                     {:port 1234
-                     :frame (string :utf-8 :delimiters ["\r\n"])
+                     :frame (string :ascii :delimiters ["\r\n"])
                      }))
--- src/irc_server/parser.clj
+++ src/irc_server/parser.clj
@@ -9,4 +9,12 @@
   (request-parser "NICK gideon\r\n")
   (request-parser "USER gideon gideon localhost :Gideon\r\n")
   (request-parser "join #foobar\r\n")
+  (request-parser "msg puddytat Hey tat, how are you?\r\n")
+  (request-parser "NOTICE gideonite wake up\r\n")
+  (request-parser "part\r\n")
+  (request-parser "partall\r\n")
+  (request-parser "ping nickname\r\n")
+  (request-parser "query you\r\n")
+  (request-parser "quit\r\n")
+  ;; ...
   )
