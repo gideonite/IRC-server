094225b03f7b29bcac174d9dbafa82796ed988a4
Gideon Dresdner <gideond@gmail.com>
2013-11-04 18:27:57 -0500

    client store: in memory map for maps {:ip :nick}

--- src/irc_server/core.clj
+++ src/irc_server/core.clj
@@ -4,13 +4,49 @@
             [lamina.core :refer :all]
             [gloss.core :refer :all]))
 
-(defn hexify [s]
-  (format "%x" (new java.math.BigInteger (.getBytes s))))
+
+;; A client is a map {:ip :nick}
+
+(defprotocol IPersistClients
+  (save-client! [store client])
+  (get-client [store client])
+  (get-nick [store nick])
+  )
+
+;;
+;; TEMPORARY CLIENTS
+;;
+;; This is a memory store for client records, an (atom {:nick :client})
+
+(defrecord TemporaryClients
+  [!cache])
+
+(defn temporary-clients []
+  (->TemporaryClients (atom {})))
+
+(extend-type TemporaryClients
+  IPersistClients
+  (save-client! [store client]
+    (swap! (:!cache store) assoc (:nick client) client))
+  (get-client [store client] (@(:!cache store) (:nick client)))
+  (get-nick [store nick] (@(:!cache store) nick)))
+
+(def clients (temporary-clients))
+
+#_
+(save-client! clients {:ip 172 :nick "foobar"})
+(get-client clients {:ip 172 :nick "foobar"})
+(get-nick clients "foobar")
+
+;;
+;; HANDLERS
+;;
 
 (defn handler [ch client-info]
   (receive-all ch
-               (fn [msg] (println (request-parser (str msg "\r\n"))) (enqueue ch "001"))
-               ))
+               (fn [msg]
+                 (println (request-parser (str msg "\r\n")))
+                 (enqueue ch "001"))))
 
 (defn start-server
   [port]
