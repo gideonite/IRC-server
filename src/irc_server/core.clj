1735038923dbf0604d54d823e58df4c95bcf53f5
Gideon Dresdner <gideond@gmail.com>
2013-11-06 11:48:52 -0500

    no need to persist server state, or think about it

--- src/irc_server/core.clj
+++ src/irc_server/core.clj
@@ -5,46 +5,14 @@
             [gloss.core :refer :all]
             [clojure.string :refer [lower-case]]))
 
-
-;; A target is a map {:ips [ips] :name name/channel}
-
-(defprotocol IPersistTargets
-  (save-target! [store target])
-  (by-name [store name])
-  )
-
-;;
-;; TEMPORARY TARGETS
-;;
-;; This is an in memory store for target records, an (atom {:name :target}).
-;; I.e. you look up by name (name/channel).
-
-(defrecord TemporaryTargets
-  [!cache])
-
-(defn temporary-targets []
-  (->TemporaryTargets (atom {})))
-
-(extend-type TemporaryTargets
-  IPersistTargets
-  (save-target! [store target]
-    (swap! (:!cache store) assoc (:name target) target))
-  (by-name [store name] (@(:!cache store) name)))
-
-(def targets (temporary-targets))
-
-(comment
-  (save-target! targets {:ips ["172.0.0.1"] :name "foobar"})
-  (save-target! targets {:ips ["123" "456.1"] :name "#foobar"})
-  (by-name targets "foobar")
-  (by-name targets "#foobar"))
-
-
 ;;
 ;; HANDLERS
 ;;
 
 (def targets (atom {}))
+
+
+;; TODO: use multimethods to dispath register on the command type
 
 (defn register!
   ([ch] (when-not (@targets ch) (swap! targets assoc ch {:channel ch})))
